1dc1375ea8375209e80bbb3bfe2c275bf3970fed
Sana-0511 <svnkanbargi@gmail.com>
2024-05-13 13:34:51 +0530

    Scavenger

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -133,7 +133,7 @@ const Navbar = () => {
                                 duration={500}
                                 onClick={homeHandler}
                             >
-                                SHOPCART
+                                SCAVENGER
                             </NavLogo>
                         </Typography>
                     </HomeContainer>
@@ -214,7 +214,7 @@ const Navbar = () => {
                             >
                                 <LocalMallIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
 
-                                SHOPCART
+                                SCAVENGER
                             </NavLogo>
                         </Typography>
                     </HomeContainer>
